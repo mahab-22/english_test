17486e7698ad02aefac4845d6a3c56e7996bc491
mahab-22 <mahab-22@mail.ru>
2021-04-06 15:18:36 +0300

    Вывод карточек

---
+++ js/index.js
@@ -0,0 +1,26 @@
+class Card {
+    constructor(out, q_arr, id) {
+        this.id = id;
+        this.q_arr = q_arr;
+        let ques = '';
+        for (let i = 1; i < q_arr.length; i++) {
+            ques += `<input type='radio' name=${this.id} value='${q_arr[i]}' id=${this.id}_${i} > <label class='card-text' for=${this.id}_${i}> ${q_arr[i]}</label><br>`
+        }
+        out.innerHTML += `<div class="card my-3 border border-secondary" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title"> ${q_arr[0]}</h5>
+            ${ques}
+        </div>
+      </div>`;
+    }
+}
+
+function loadCaards() {
+    let out = document.querySelector('.cardspace');
+    let card_ = [];
+    for (let i = 0; i < questions.length; i++) {
+        card_[i] = new Card(out, questions[i], i);
+    }
+
+};
+loadCaards();
